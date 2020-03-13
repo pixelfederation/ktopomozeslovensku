ee3a74b4fd4937055ea9bb163c71d9273343ff16
Tomas Mihalicka <tmihalicka@pixelfederation.com>
2020-03-13 16:31:02 +0100

    added policy screen

---
+++ src/Controller/PolicyController.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * @author tmihalicka
+ * @copyright PIXEL FEDERATION
+ * @license Internal use only
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment as Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ *
+ */
+final class PolicyController
+{
+    /**
+     * @var Twig
+     */
+    private $twig;
+
+    /**
+     * @param Twig $twig
+     */
+    public function __construct(Twig $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @return Response
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('policy.html.twig'));
+    }
+}
---
+++ src/Service/TransparentAccountReporterService.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * @author tmihalicka
+ * @copyright PIXEL FEDERATION
+ * @license Internal use only
+ */
+
+namespace App\Service;
+
+/**
+ *
+ */
+final class TransparentAccountReporterService
+{
+
+}
