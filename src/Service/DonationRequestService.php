7b1641079117436b1b37028f59f91551aee5c843
Tomas Mihalicka <tmihalicka@pixelfederation.com>
2020-03-14 16:22:44 +0100

    added mail to donation request

--- src/Service/DonationRequestService.php
+++ src/Service/DonationRequestService.php
@@ -23,11 +23,17 @@ final class DonationRequestService
     private $entityManager;
 
     /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    /**
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, Mailer $mailer)
     {
         $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
     }
 
     /**
@@ -40,5 +46,13 @@ final class DonationRequestService
         $this->entityManager->persist($request);
         $this->entityManager->flush();
         $this->entityManager->clear();
+
+        $this->mailer->sendMail('Bol prijatý nový dar.',
+            "Meno kontaktnej osoby: {$request->getContactPerson()}" . PHP_EOL
+            . "Adresa konktaktnej osoby: {$request->getAddress()}" . PHP_EOL
+            . "Telefónne číslo: {$request->getTelephone()}" . PHP_EOL
+            . "E-mail adresa: {$request->getEmail()}" . PHP_EOL
+            . "Typ pomôcok, ktoré viem ponúknuť: {$request->getDonationItem()->getName()}" . PHP_EOL
+            . "Množstvo: {$request->getQuantity()}" . PHP_EOL );
     }
 }
