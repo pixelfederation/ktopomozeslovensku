c5790b61fce69977f04852f3dc7b23e0df06139b
mfris <mfris@pixelfederation.com>
2020-03-17 18:50:59 +0100

    fixed migration

--- src/Migrations/Version20200317175032.php
+++ src/Migrations/Version20200317175032.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200317174406 extends AbstractMigration
+final class Version20200317175032 extends AbstractMigration
 {
     public function getDescription() : string
     {
