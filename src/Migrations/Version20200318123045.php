571a709fc84ba09403be2d1319cb68a3bdcc0d83
Tomas Mihalicka <tmihalicka@pixelfederation.com>
2020-03-18 14:40:22 +0100

    merged dev + fixed entities added forms

---
+++ src/Migrations/Version20200318123045.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200318123045 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE help_request DROP request_text');
+        $this->addSql('ALTER TABLE danation_requests_items ADD other LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE help_requests_items ADD other LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE danation_requests_items DROP other');
+        $this->addSql('ALTER TABLE help_request ADD request_text LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE help_requests_items DROP other');
+    }
+}
