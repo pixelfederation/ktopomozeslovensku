59e277bd24e5b53069a17ea6280a4be6bbcf7b62
Peter Brecska <pbrecska@pixelfederation.com>
2020-03-21 21:11:22 +0100

    entites

---
+++ src/Entity/RecipientGroup.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+/*
+ * @author pbrecska <pbrecska@pixelfederation.com>
+ * @copyright PIXEL FEDERATION
+ * @license Internal use only
+ */
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="recipient_group",
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="recipient_group", columns={"name"})})
+ */
+class RecipientGroup
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", name="id")
+     */
+    private $id;
+
+    /**
+     * @var Collection
+     *
+     * @ORM\OneToMany(targetEntity="App\Entity\Recipient", mappedBy="recipientGroup", cascade={"persist","remove"})
+     */
+    private $recipients;
+
+    /**
+     * @var String
+     * @ORM\Column(type="string", length=250, name="name")
+     */
+    private $name = '';
+
+    public function __construct()
+    {
+        $this->recipients = new ArrayCollection();
+    }
+
+    /**
+     * @return String
+     */
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getRecipients(): Collection
+    {
+        return $this->recipients;
+    }
+
+    /**
+     * @return String
+     */
+    public function getName(): String
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param Collection $recipients
+     *
+     * @return void
+     */
+    public function setRecipients(Collection $recipients): void
+    {
+        $this->recipients = $recipients;
+    }
+
+    /**
+     * @param String $name
+     *
+     * @return void
+     */
+    public function setName(String $name): void
+    {
+        $this->name = $name;
+    }
+}
