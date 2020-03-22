087728868564d5f8bcc6db60e42b93ef710729aa
Peter Brecska <pbrecska@pixelfederation.com>
2020-03-22 11:20:42 +0100

    fix migration Version20200321193055

--- src/Migrations/Version20200321193055.php
+++ src/Migrations/Version20200321193055.php
@@ -25,16 +25,13 @@ final class Version20200321193055 extends AbstractMigration
         $this->addSql('ALTER TABLE recipient ADD recipient_group_id INT DEFAULT NULL;');
      $this->addSql('ALTER TABLE recipient ADD CONSTRAINT FK_6804FB49104C8843 FOREIGN KEY (recipient_group_id) REFERENCES recipient_group (id);');
      $this->addSql('CREATE INDEX IDX_6804FB49104C8843 ON recipient (recipient_group_id);');
-     $this->addSql('ALTER TABLE donation_requests_items RENAME INDEX idx_dbad424de04907ed TO IDX_101F3ECDE04907ED;');
-     $this->addSql('ALTER TABLE donation_requests_items RENAME INDEX idx_dbad424d126f525e TO IDX_101F3ECD126F525E;');
 
     }
 
     public function down(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('DROP INDEX IDX_101F3ECDE04907ED ON donation_requests_items');
-        $this->addSql('DROP INDEX IDX_101F3ECD126F525E ON donation_requests_items');
+        $this->addSql('DROP INDEX IDX_6804FB49104C8843 ON recipient');
         $this->addSql('DROP TABLE recipient_group;');
 
     }
